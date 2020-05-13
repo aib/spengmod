920907e9174cae41b01a79464d9021b6ccd80af5
aib <aibok42@gmail.com>
2020-05-13 13:51:35 +0300

    Add TextPanelTerminal for using text panels as scrolling terminals

---
+++ util/TextPanelTerminal.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Text;
+using Sandbox.ModAPI.Ingame;
+
+public
+class TextPanelTerminal
+{
+	public readonly IMyTextPanel display;
+
+	private int maxLines;
+
+	public TextPanelTerminal(IMyTextPanel display, int lines = 1)
+	{
+		this.display = display;
+		setLines(lines);
+	}
+
+	public void setLines(int lines)
+	{
+		this.maxLines = lines;
+		refresh();
+	}
+
+	public void refresh()
+	{
+		var sb = new StringBuilder();
+		var lines = display.GetText().Split('\n');
+
+		for (int i = Math.Max(lines.Length - maxLines, 0); i < lines.Length; i++) {
+			sb.AppendLine(lines[i]);
+		}
+
+		display.WriteText(sb);
+	}
+
+	public void addLine(string line)
+	{
+		display.WriteText(line, true);
+		refresh();
+	}
+}
