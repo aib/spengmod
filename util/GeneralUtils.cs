30fabd34d5b39c8a4194fd0b86fa225cffe38259
aib <aibok42@gmail.com>
2020-05-18 22:27:16 +0300

    Add colorChar()

--- util/GeneralUtils.cs
+++ util/GeneralUtils.cs
@@ -6,4 +6,7 @@ public static class GeneralUtils {
 public static
 List<T> getList<T>(Action<List<T>> f) { var l = new List<T>(); f(l); return l; }
 
+public static
+char colorChar(int r, int g, int b) { return (char)('\xe100' + (r << 6) + (g << 3) + b); }
+
 }
